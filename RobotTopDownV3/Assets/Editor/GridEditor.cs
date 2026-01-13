f0573bfb7b40a764b4fb01aeb589d4b3aadde7e1
Julien Haigron <julien@pinpinteam.com>
2026-01-13 12:41:31 +0100

    add true grid editor tool and window

--- RobotTopDownV3/Assets/Editor/GridEditor.cs
+++ RobotTopDownV3/Assets/Editor/GridEditor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using Sirenix.OdinInspector;
+using UnityEditor.EditorTools;
 
 [CustomEditor(typeof(GridManager))]
 public class GridEditor : Editor
@@ -51,7 +52,7 @@ public class GridEditor : Editor
 		}
 	}
 
-	private void OnSceneGUI ()
+	/*private void OnSceneGUI ()
 	{
 		// remember to toggle gizmos in scene
 		// and to deactivate interaction to GameManger prefab in scene
@@ -110,5 +111,91 @@ public class GridEditor : Editor
 				}
 			}
 		}
+	}*/
+}
+
+[EditorTool("Grid Tool")]
+public class GridTool : EditorTool
+{
+	private bool m_hasReleaseKey = true;
+
+	public override GUIContent toolbarIcon => new GUIContent(
+		EditorGUIUtility.IconContent("d_EditCollider").image,
+		"Grid Tool"
+	);
+
+	public override void OnToolGUI ( EditorWindow window )
+	{
+		if (!GridManager.Instance.isGroundBrushSelected)
+			return;
+
+		Event e = Event.current;
+
+		if (e.type == EventType.MouseDown && e.keyCode == KeyCode.Mouse0)
+		{
+			Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+			if (Physics.Raycast(ray, out RaycastHit hitInfo, GameConfig.current.input.interactionRayCastLength, GameConfig.current.input.interactionRayCastLayer))
+			{
+				if (hitInfo.transform.parent.TryGetComponent(out Tile tile))
+				{
+					tile.SetGroundType(GridManager.Instance.currentGroundBrushSelected);
+					EditorUtility.SetDirty(this);
+				}
+			}
+		}
+
+		if (!m_hasReleaseKey && e.type == EventType.KeyDown)
+			return;
+		else if (e.type == EventType.KeyUp)
+		{
+			m_hasReleaseKey = true;
+			return;
+		}
+		else if (m_hasReleaseKey && e.type == EventType.KeyDown)
+			m_hasReleaseKey = false;
+
+		if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Alpha1)
+		{
+			Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+			if (Physics.Raycast(ray, out RaycastHit hitInfo, GameConfig.current.input.interactionRayCastLength, GameConfig.current.input.interactionRayCastLayer))
+			{
+				if (hitInfo.transform.parent.TryGetComponent(out Tile tile))
+				{
+					tile.Wall.RotateRight();
+				}
+			}
+		}
+
+		if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Alpha2)
+		{
+			Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+			if (Physics.Raycast(ray, out RaycastHit hitInfo, GameConfig.current.input.interactionRayCastLength, GameConfig.current.input.interactionRayCastLayer))
+			{
+				if (hitInfo.transform.parent.TryGetComponent(out Tile tile))
+				{
+					Wall.WallType nextWallType = (Wall.WallType)((int)++tile.Wall.Type % (int)Wall.WallType.Total);
+					tile.Wall.SetWallType(nextWallType);
+				}
+			}
+		}
+		/*Event e = Event.current;
+
+		if (e.type == EventType.MouseDown && e.button == 0)
+		{
+			Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+
+			if (Physics.Raycast(ray, out var hit))
+			{
+				Selection.activeGameObject = hit.collider.gameObject;
+				e.Use();
+			}
+		}
+
+		// Exemple de gizmo
+		Handles.color = Color.cyan;
+		Handles.DrawWireDisc(Vector3.zero, Vector3.up, 5f);*/
 	}
 }
