6a69f15904aa15079b14fd9e9f397518fb07cfcd
Julien Haigron <unjourpeuxtre@gmail.com>
2026-06-25 16:40:16 +0200

    solo hub panel

--- RobotTopDownV3/Assets/Scripts/UI/Panel/SoloHubPanel.cs
+++ RobotTopDownV3/Assets/Scripts/UI/Panel/SoloHubPanel.cs
@@ -5,47 +5,22 @@ using Sirenix.OdinInspector;
 
 public class SoloHubPanel : AUIPanel
 {
-	[SerializeField] private BaseButton m_changeSquadBtn;
-	[SerializeField] private BaseButton m_openShopBtn;
+	[SerializeField] private BaseButton m_hangarBtn;
+	[SerializeField] private SerializableDictionary<EntityEquipmentData.EntityFaction, BaseButton> m_openShopBtns;
 	[SerializeField] private BaseButton m_recycleShopBtn;
 	[SerializeField] private BaseButton m_repairBtn;
 	[SerializeField] private BaseButton m_missionBtn;
+	[SerializeField] private BaseButton m_tournamentBtn;
 
 	private void Awake ()
 	{
-		m_changeSquadBtn.onClick += OnClickChangeSquadBtn;
-		m_openShopBtn.onClick += OnClickOpenShopBtn;
+		m_hangarBtn.onClick += OnClickHangarBtn;
+		foreach(KeyValuePair< EntityEquipmentData.EntityFaction, BaseButton> shopBtn in m_openShopBtns)
+			shopBtn.Value.onClick += () => OnClickOpenShopBtn(shopBtn.Key);
 		m_recycleShopBtn.onClick += OnClickOpenRecycleBtn;
 		m_repairBtn.onClick += OnClickOpenRepairBtn;
 		m_missionBtn.onClick += OnClickMissionBtn;
-	}
-
-	private void OnClickChangeSquadBtn ()
-	{
-		UIManager.Instance.OpenPanel<HangarPanel>();
-	}
-	
-	private void OnClickOpenRecycleBtn ()
-	{
-		UIManager.Instance.OpenPanel<RecyclePanel>().Init();
-	}
-
-	private void OnClickOpenShopBtn ()
-	{
-		UIManager.Instance.OpenPanel<ShopPanel>().Init(EntityEquipmentData.EntityFaction.Psy);
-	}
-
-	private void OnClickOpenRepairBtn ()
-	{
-		UIManager.Instance.OpenPanel<RepairStationPanel>().Init();
-	}
-	
-	private void OnClickMissionBtn ()
-	{
-		if(GameDatas.current.currentPlayerSave.dayCount <= 3)
-			UIManager.Instance.OpenPanel<MissionPanel>();
-		else
-			UIManager.Instance.OpenPanel<TournamentPanel>();
+		m_tournamentBtn.onClick += OnClickTournamentBtn;
 	}
 
 	protected override void OnShowStarted ()
@@ -56,7 +31,49 @@ public class SoloHubPanel : AUIPanel
 
 	private void RefreshVisual ()
 	{
-		m_missionBtn.SetInteractability(GameManager.Instance.SquadValidityPredicate());
+		bool isSquadValid = GameManager.Instance.SquadValidityPredicate();
+		m_missionBtn.SetInteractability(isSquadValid);
+		m_tournamentBtn.SetInteractability(isSquadValid);
 	}
+
+	#region Callbacks
+
+	private void OnClickHangarBtn ()
+	{
+		UIManager.Instance.OpenPanel<HangarPanel>();
+	}
+	
+	private void OnClickOpenRecycleBtn ()
+	{
+		UIManager.Instance.OpenPanel<RecyclePanel>().Init();
+	}
+
+	private void OnClickOpenShopBtn ( EntityEquipmentData.EntityFaction _faction)
+	{
+		UIManager.Instance.OpenPanel<ShopPanel>().Init(_faction);
+	}
+
+	private void OnClickOpenRepairBtn ()
+	{
+		UIManager.Instance.OpenPanel<RepairStationPanel>().Init();
+	}
+	
+	private void OnClickMissionBtn ()
+	{
+		if (GameDatas.current.currentPlayerSave.dayCount > 3)
+			return;
+
+		UIManager.Instance.OpenPanel<MissionPanel>();
+	}
+
+	private void OnClickTournamentBtn ()
+	{
+		if (GameDatas.current.currentPlayerSave.dayCount <= 3)
+			return;
+
+		UIManager.Instance.OpenPanel<TournamentPanel>();
+	}
+
+	#endregion
 
 }
