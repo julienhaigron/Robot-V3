e38368b7906be5c2c9bcc042d98db3c2465b5bb5
Julien Haigron <unjourpeuxtre@gmail.com>
2026-07-11 17:58:44 +0200

    fix action queue

--- RobotTopDownV3/Assets/Scripts/UI/Misc/EntityActionDisplay.cs
+++ RobotTopDownV3/Assets/Scripts/UI/Misc/EntityActionDisplay.cs
@@ -8,8 +8,8 @@ public class EntityActionDisplay : MonoBehaviour
 	[Title("Dependencies")]
 	[SerializeField] private Image m_backgroundImg;
 	[SerializeField] private Transform m_actionIconParent;
+	[SerializeField] private Transform m_actionIconPivot;
 	[SerializeField] private Image m_actionIconImg;
-	[SerializeField] private TextMeshProUGUI m_actionTmp;
 	[SerializeField] private Image m_mainHachureLeft;
 	[SerializeField] private Image m_mainHachureRight;
 	[SerializeField] private Image m_preparationHachure;
@@ -18,24 +18,26 @@ public class EntityActionDisplay : MonoBehaviour
 	private TurnManager.RecordedAction m_recordedAction;
 	public TurnManager.RecordedAction RecordedAction => m_recordedAction;
 
-	[Title("Parameters")]
+	[Title("Background Parameters")]
 	[SerializeField] private float m_unitLenght = 135.4f;
 	[SerializeField] private float m_offsetLenght = 2.2f;
 	[SerializeField] private float m_firstUnitStartOffset = 15f;
 
-	[SerializeField] private float m_iconBaseXPosition = 59.2f;
-	[SerializeField] private float m_iconXOffsetPosition = 142f;
+	[Title("Icon Parameters")]
+	[SerializeField] private float m_iconBaseXPosition = 74.5f;
+	[SerializeField] private float m_iconOffsetBaseXPosition = -11f;
+	[SerializeField] private float m_iconFirstORLastElemOffset = 7.5f;
+	[SerializeField] private float m_iconTimeFactorWidth = 136.87f;
 
+	[Title("Hatched Parameters")]
 	[SerializeField] private float m_baseGreenHatchedWidth = 69f;
-	[SerializeField] private float m_greenHatchedOneTickWidth = 117.1f;
-	[SerializeField] private float m_redHatchedOneTickWidth = 117.1f;
+	[SerializeField] private float m_greenHatchedOneTickWidth = 134.2f;
+	[SerializeField] private float m_redHatchedOneTickWidth = 134.2f;
 	[SerializeField] private float m_greenHatchedOneActivationAndHasCooldownOrPreparationOffset = 9.4f;
 
 	public void Init ( TurnManager.RecordedAction _recordedAction )
 	{
 		m_recordedAction = _recordedAction;
-		m_actionTmp.text = GameAssets.current.game.entityActionsData[_recordedAction.type].displayName;
-
 		RefreshVisual(m_recordedAction.action.timeAtStart, m_recordedAction.action.TotalDuration, m_recordedAction.action.preparationDuration, m_recordedAction.action.cooldownDuration);
 
 		Show(false);
@@ -56,26 +58,27 @@ public class EntityActionDisplay : MonoBehaviour
 			+ (_timeAtStart > 0 ? m_firstUnitStartOffset : 0f);
 		(m_backgroundImg.transform as RectTransform).anchoredPosition = newPos;
 
-		//icon pos
-		Vector2 newIconPos = (m_actionIconImg.transform as RectTransform).anchoredPosition;
-		newIconPos.x = m_iconBaseXPosition + (m_iconXOffsetPosition * _preparationTime + ((float)_totalDuration / 2f));
-		(m_actionIconParent as RectTransform).anchoredPosition = newIconPos;
-
-		//hachuré
-
-		//here
-		//TODO :
-		//- icon position is wrong => need to redo formula because preparation lenght doesnt seems to be taken into account
-		//- green hatched width formula is wrong when actualDuration % 2 == 0
-
 		float activeDuration = _totalDuration - _preparationTime - _cooldownTime;
 
-		Vector2 leftGreenHatchedWidth = (m_mainHachureLeft.transform as RectTransform).sizeDelta;
-		leftGreenHatchedWidth.x = m_baseGreenHatchedWidth
-			+ (activeDuration > 1 ? (m_greenHatchedOneTickWidth * ((float)activeDuration / 2f)) : 0f)
-			/*+ (activeDuration == 1 && _preparationTime > 0 ? m_greenHatchedOneActivationAndHasCooldownOrPreparationOffset : 0f)*/;
-		(m_mainHachureLeft.transform as RectTransform).sizeDelta = leftGreenHatchedWidth;
+		//icon pos
+		float timeFactor = _preparationTime + ((activeDuration / 2f));
+		Vector2 newIconPos = (m_actionIconParent.transform as RectTransform).anchoredPosition;
+		newIconPos.x = (activeDuration > 1 || _preparationTime > 0 ? m_iconTimeFactorWidth * timeFactor : m_iconBaseXPosition);
+		(m_actionIconParent as RectTransform).anchoredPosition = newIconPos;
+		
+		//icon offset
+		Vector2 newIconPos2 = (m_actionIconPivot.transform as RectTransform).anchoredPosition;
+		newIconPos2.x = m_iconOffsetBaseXPosition
+			- (_timeAtStart != 0 && (_timeAtStart + _totalDuration != 10) ? m_iconFirstORLastElemOffset : 0f);
+		(m_actionIconPivot as RectTransform).anchoredPosition = newIconPos2;
 
+		//hachuré
+		float greenHatchedWidth = (activeDuration > 1 ? (m_greenHatchedOneTickWidth * ((float)activeDuration / 2f)) : m_baseGreenHatchedWidth);
+
+		Vector2 leftGreenHatchedWidth = (m_mainHachureLeft.transform as RectTransform).sizeDelta;
+		leftGreenHatchedWidth.x = greenHatchedWidth;
+			/*+ (activeDuration == 1 && _preparationTime > 0 ? m_greenHatchedOneActivationAndHasCooldownOrPreparationOffset : 0f);*/
+		(m_mainHachureLeft.transform as RectTransform).sizeDelta = leftGreenHatchedWidth;
 
 		if (_preparationTime > 0)
 		{
@@ -88,15 +91,12 @@ public class EntityActionDisplay : MonoBehaviour
 		else
 			m_preparationHachure.gameObject.SetActive(false);
 
-
 		m_mainHachureRight.gameObject.SetActive(true);
 		Vector2 rightGreenHatchedWidth = (m_mainHachureRight.transform as RectTransform).sizeDelta;
-		rightGreenHatchedWidth.x = m_baseGreenHatchedWidth
-			+ (activeDuration > 1 ? (m_greenHatchedOneTickWidth * ((float)activeDuration / 2f)) : 0f)
-			- (_timeAtStart != 0 && (_timeAtStart + _totalDuration != 10) && activeDuration == 1 && _cooldownTime == 0 ? m_firstUnitStartOffset : 0f)
-			+ (activeDuration == 1 && _cooldownTime > 0 ? m_greenHatchedOneActivationAndHasCooldownOrPreparationOffset : 0f);
+		rightGreenHatchedWidth.x = greenHatchedWidth
+			- (_timeAtStart != 0 && (_timeAtStart + _totalDuration != 10) && activeDuration == 1 && _cooldownTime == 0 ? m_firstUnitStartOffset : 0f);
+			//+ (activeDuration == 1 && _cooldownTime > 0 ? m_greenHatchedOneActivationAndHasCooldownOrPreparationOffset : 0f);
 		(m_mainHachureRight.transform as RectTransform).sizeDelta = rightGreenHatchedWidth;
-
 
 		if (_cooldownTime > 0)
 		{
