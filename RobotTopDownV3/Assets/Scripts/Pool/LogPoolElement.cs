edfaeb2f8534c8ea5ed684a4fcef6ccbeb53c486
Julien Haigron <unjourpeuxtre@gmail.com>
2026-08-03 20:25:32 +0200

    opti log console

--- RobotTopDownV3/Assets/Scripts/Pool/LogPoolElement.cs
+++ RobotTopDownV3/Assets/Scripts/Pool/LogPoolElement.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class LogPoolElement : PoolElement
 {
     [SerializeField] private TextMeshProUGUI m_text;
+	[SerializeField] private LayoutElement m_layoutElement;
 
-    public void Init(string _content )
+	public void Init(string _content )
 	{
 		m_text.text = _content;
-	}
+        m_text.ForceMeshUpdate();
+
+        float preferredHeight = m_text.GetPreferredValues().y;
+        m_layoutElement.preferredHeight = preferredHeight + 8f;
+    }
 }
