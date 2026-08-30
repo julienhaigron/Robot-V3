96faad50026a3421699783c3839d86af89e5b150
julien <julien@pinpinteam.com>
2026-08-30 18:18:03 +0200

    wip localization integration

--- RobotTopDownV3/Assets/Scripts/Localization/LocalizationKey.cs
+++ RobotTopDownV3/Assets/Scripts/Localization/LocalizationKey.cs
@@ -2,15 +2,27 @@
 public enum LocalizationKey
 {
     menu_audio,
+    menu_multiplayer,
+    menu_continue,
+    menu_quit,
+    menu_return,
+    menu_new_game,
+    menu_options,
 }
 
 public static class LocalizationKeyExtensions
 {
-    public static string ToKey(this LocalizationKey _key)
+    public static string ToKey(this LocalizationKey key)
     {
-        switch(_key)
+        switch(key)
         {
             case LocalizationKey.menu_audio: return "menu/audio";
+            case LocalizationKey.menu_multiplayer: return "menu/multiplayer";
+            case LocalizationKey.menu_continue: return "menu/continue";
+            case LocalizationKey.menu_quit: return "menu/quit";
+            case LocalizationKey.menu_return: return "menu/return";
+            case LocalizationKey.menu_new_game: return "menu/new_game";
+            case LocalizationKey.menu_options: return "menu/options";
             default: return string.Empty;
         }
     }
