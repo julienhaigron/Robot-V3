2b7724c9ea5bf1dce58a01bf594a23fa97514274
Julien Haigron <unjourpeuxtre@gmail.com>
2026-09-11 23:54:11 +0200

    fix ftue

--- RobotTopDownV3/Assets/Scripts/Tasking/FTUEManager.cs
+++ RobotTopDownV3/Assets/Scripts/Tasking/FTUEManager.cs
@@ -207,7 +207,7 @@ public class FTUEManager : Singleton<FTUEManager>
 		tutoSequence.Append(new OpenPanelTask<HangarPanel>("Send player directly to hangar", ( context ) => context.UI.currentPanel is SoloHubPanel
 			&& GameDatas.current.currentPlayerSave.dayCount == 0));
 		tutoSequence.Append(new DialogueHighlightTask("New unit won explenation", ( context ) => context.UI.currentPanel is HangarPanel
-		, m_day1TutoDialogues[0], "squadUnits"));
+		, m_day1TutoDialogues[0], "squadUnit2").SetSkipPredicate(( context ) => context.UI.currentPanel is SoloHubPanel));
 		tutoSequence.Append(new DialogueHighlightTask("Hub presentation", ( context ) => context.UI.currentPanel is SoloHubPanel
 		, m_day1TutoDialogues[1], "missionSection"));
 		tutoSequence.Append(new DialogueHighlightTask("Available matches explenation", ( context ) => context.UI.currentPanel is MissionPanel
